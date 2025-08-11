7e988d5ee75800afa160b110bfa5f968c8ef9712
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-08-11 23:09:40 +0300

    In setCommandMap(), Implemented the majority of commands for the 2nd half of robot states (states that invlove an algae)

--- src/main/java/frc/robot/StateMachine.java
+++ src/main/java/frc/robot/StateMachine.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.lib.NinjasLib.statemachine.StateMachineBase;
 
@@ -87,28 +89,74 @@ public class StateMachine extends StateMachineBase<States> {
         //endregion
 
         //region outtake coral
-        addCommand(States.PREPARE_CORAL_OUTTAKE_LOW, Commands.none());
-        addCommand(States.CORAL_OUTTAKE_LOW, Commands.none());
-        addCommand(States.ARM_INTAKE, Commands.none());
+        addCommand(States.PREPARE_CORAL_OUTTAKE_LOW, Commands.sequence(
+                intakeAngle.setPercent(() -> 0.5)
+                //TODO: implement angle check
+//                Commands.waitUntil(() -> {
+//                    RobotContainer.getIntakeAngle()
+//                }),
+        ));
+        addCommand(States.CORAL_OUTTAKE_LOW, Commands.sequence(
+                intake.outputCoral(),
+                Commands.waitSeconds(0.2),
+                intakeAngle.setPercent(() -> 0),
+                Commands.waitSeconds(0.2),
+                Commands.runOnce(()-> changeRobotState(States.CLOSE))
+        ));
+        addCommand(States.ARM_INTAKE, Commands.sequence(
+                 //TODO: implement angle check,
+                outtake.intakeCoral(),
+                Commands.waitUntil(arm::isCoralInside),
+                Commands.runOnce(()-> changeRobotState(States.CORAL_IN_ARM))
+        ));
         addCommand(States.CORAL_IN_ARM, Commands.none());
         addCommand(States.PREPARE_CORAL_OUTTAKE_HIGH, Commands.none());
         addCommand(States.CORAL_OUTTAKE_HIGH, Commands.none());
         //endregion
 
         //region intake algae
-        addCommand(States.INTAKE_ALGAE_LOW, Commands.none());
-        addCommand(States.INTAKE_ALGAE_HIGH, Commands.none());
+        addCommand(States.INTAKE_ALGAE_LOW, Commands.sequence(
+                arm.setAngle(Rotation2d.kZero),
+                Commands.waitUntil(arm::atGoal),
+                outtake.intakeCoral(),
+                Commands.waitUntil(arm::isCoralInside),
+                Commands.runOnce(()-> changeRobotState(States.ALGAE_IN_ARM))
+                // No need to stop the intaking because we want to keep ahold of the algae. we'll stop only when we outtake.
+        ));
+        addCommand(States.INTAKE_ALGAE_HIGH, Commands.sequence(
+                Commands.runOnce(()-> changeRobotState(States.ALGAE_IN_ARM))
+                // No need to stop the intaking because we want to keep ahold of the algae. we'll stop only when we outtake.
+        ));
         addCommand(States.ALGAE_IN_ARM, Commands.none());
         //endregion
 
         //region outtake algae
         addCommand(States.PREPARE_ALGAE_OUTTAKE, Commands.none());
-        addCommand(States.ALGAE_OUTTAKE, Commands.none());
+        addCommand(States.ALGAE_OUTTAKE, Commands.sequence(
+                outtake.outtakeAlgae(),
+                Commands.waitSeconds(0.2),
+                outtake.stop(),
+                Commands.runOnce(()-> changeRobotState(States.CLOSE))
+        ));
         //endregion
 
         //region close + reset
-        addCommand(States.CLOSE, Commands.none());
-        addCommand(States.RESET, Commands.none());
+        addCommand(States.CLOSE, Commands.sequence(
+                Commands.parallel(
+                        arm.setAngle(Rotation2d.kZero),
+                        elevator.setHeight(() -> 0),
+                        //TODO: implement angle check,
+                        intake.setPercent(()->0)
+                ),
+                Commands.waitUntil(() -> arm.atGoal() && elevator.atGoal()),
+                Commands.runOnce(()-> changeRobotState(States.IDLE))
+        ));
+        addCommand(States.RESET, Commands.sequence(
+                elevator.reset(),
+                arm.reset(),
+                Commands.waitUntil(() -> elevator.isReset() && arm.isReset()),
+                Commands.runOnce(()-> changeRobotState(States.IDLE))
+        ));
         //endregion
     }
 }
