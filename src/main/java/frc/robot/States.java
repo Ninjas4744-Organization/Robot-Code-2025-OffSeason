4ee3d3ecd06bffc9df74c7e29571cc41354ab17f
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-08-11 18:39:21 +0300

    Added the different states the robot can be in and added the skeleton for the StateMachine.java file.

--- src/main/java/frc/robot/States.java
+++ src/main/java/frc/robot/States.java
@@ -2,5 +2,22 @@ package frc.robot;
 
 public enum States {
     IDLE,
-    SIGMA
+
+    INTAKE_CORAL,
+    CORAL_IN_INTAKE, //the coral is set in the middle of the intake
+    PREPARE_CORAL_OUTTAKE_LOW, //For L1
+    CORAL_OUTTAKE_LOW,
+    ARM_INTAKE, //transfer the coral from the intake from the bottom to the arm
+    CORAL_IN_ARM,//the coral is set in the arm
+    PREPARE_CORAL_OUTTAKE_HIGH, //For L2-L4
+    CORAL_OUTTAKE_HIGH,
+
+    PREPARE_INTAKE_ALGAE_HIGH, //grabbing algae from the reef
+    PREPARE_INTAKE_ALGAE_LOW, //grabbing algae from the floor
+    INTAKE_ALGAE, //Grabbing algae from the floor or the reef
+    PREPARE_ALGAE_OUTTAKE, //towards the barge
+    ALGAE_OUTTAKE,
+
+    CLOSE,
+    RESET
 }
